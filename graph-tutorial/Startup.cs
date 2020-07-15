393761cb16524ca0d82759900f836ca19173b3a6
gczibik <gabor.czibik@partner.qualysoft.com>
2020-07-15 14:53:00 +0200

    Build ASP.NET MVC web applications with the Microsoft Graph .NET SDK

--- graph-tutorial/Startup.cs
+++ graph-tutorial/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
 
 using Owin;
 
@@ -9,11 +6,11 @@ using Owin;
 
 namespace graph_tutorial
 {
-    public class Startup
+    public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigureAuth(app);
         }
     }
 }
